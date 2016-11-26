837b47c364df166c558910b089ea5abd375f239a
Saturn91 <manuel.geissberger@gmail.com>
2016-11-26 11:23:29 +0100

    [ADD] Background + Decor - Fix Particles

---
+++ JetPackJoyRacer/Assets/Skripts/FixParticleEffectToLayer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FixParticleEffectToLayer : MonoBehaviour {
+
+    private ParticleSystem ps;
+
+	// Use this for initialization
+	void Start () {
+        ps = GetComponent<ParticleSystem>();
+        Renderer renderer = ps.GetComponent<Renderer>();
+        renderer.sortingLayerName = "Player";
+        renderer.sortingOrder = -1; //prevents the particels to spawn above the player (because its in the same Layer!)
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
