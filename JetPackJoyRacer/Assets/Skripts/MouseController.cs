48455224596058a12cc1556c4b77160b192667de
Saturn91 <manuel.geissberger@gmail.com>
2016-11-28 23:07:58 +0100

    [ADD] Animation

--- JetPackJoyRacer/Assets/Skripts/MouseController.cs
+++ JetPackJoyRacer/Assets/Skripts/MouseController.cs
@@ -5,12 +5,18 @@ public class MouseController : MonoBehaviour {
 
     public float jetpackForce = 75.0f;
     public float forwardMovementSpeed = 3.0f;
+    public Transform groundCheckTransform;
+    public LayerMask groundCheckLayerMask;
 
     private Rigidbody2D rigidbody_2D;
+    private bool grounded;
+    
+    private Animator animator;
 
     // Use this for initialization
     void Start () {
         rigidbody_2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     void FixedUpdate()
@@ -25,5 +31,16 @@ public class MouseController : MonoBehaviour {
         Vector2 newVelocity = rigidbody_2D.velocity;
         newVelocity.x = forwardMovementSpeed;
         rigidbody_2D.velocity = newVelocity;
+
+        UpdateGroundedStatus();
+    }
+
+    void UpdateGroundedStatus()
+    {
+        //1
+        grounded = Physics2D.OverlapCircle(groundCheckTransform.position, 0.1f, groundCheckLayerMask);
+
+        //2
+        animator.SetBool("grounded", grounded);
     }
 }
